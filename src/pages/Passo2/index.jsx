fc63fe578735f49353dcf1c447b7d56e5c32ef82
Julio Gaiotto <julio.gaiotto@totvs.com.br>
2021-02-28 15:58:47 -0300

    estrtura da tela de tipos de massa

--- src/pages/Passo2/index.jsx
+++ src/pages/Passo2/index.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import StepsMenu from '../../components/StepsMenu';
 
-import ContainerGeneral from './styles';
+import { ContainerGeneral, ContainerMassas, Item } from './styles';
 import Title from '../../components/Title';
 
-const Passo2 = () => (
-  <ContainerGeneral>
-    <StepsMenu page={2} />
-    <Title text="Escolha a massa que usaremos em sua pizza." />
-  </ContainerGeneral>
-);
+const Passo2 = () => {
+  const [sizes, setSizes] = useState([]);
+
+  useEffect(() => {
+    setSizes([
+      {
+        id: 1,
+        name: 'Massa napolitana',
+      },
+      {
+        id: 2,
+        name: 'Massa nova-iorquina',
+      },
+      {
+        id: 3,
+        name: 'Massa siciliana',
+      },
+    ]);
+  }, []);
+
+  return (
+    <ContainerGeneral>
+      <StepsMenu page={2} />
+      <Title text="Escolha o tipo de massa que usaremos em sua pizza." />
+
+      <ContainerMassas>
+        {sizes.map((e) => (
+          <Item key={e.id}>
+            <p>{e.name}</p>
+          </Item>
+        ))}
+      </ContainerMassas>
+    </ContainerGeneral>
+  );
+};
 
 export default Passo2;
