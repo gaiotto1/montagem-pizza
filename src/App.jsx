bcb5b42445f7e09303926e970faf494018ff50b3
Julio Gaiotto <julio.gaiotto@totvs.com.br>
2021-03-01 07:13:25 -0300

    configurando o redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from './styles/global';
-
 import Routes from './routes';
+
+import store from './store';
 
 const App = () => (
   <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-    <GlobalStyle />
-    <ToastContainer autoClose={4000} />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+      <ToastContainer autoClose={4000} />
+    </Provider>
   </>
 );
 
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import pedido from './pedido/reducer';
+
+export default combineReducers({
+  pedido,
+});
