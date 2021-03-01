76e42bbfe4ca67595e0b37eeebeefab47697017f
Julio Gaiotto <julio.gaiotto@totvs.com.br>
2021-03-01 06:46:31 -0300

    buscando recheios ao abrir a tela

--- src/pages/Passo3/index.jsx
+++ src/pages/Passo3/index.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-dynamic-require */
 import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import api from '../../services/api';
 
 import StepsMenu from '../../components/StepsMenu';
 
@@ -9,45 +11,20 @@ import Title from '../../components/Title';
 const Passo3 = () => {
   const [menu, setMenu] = useState([]);
 
+  async function getRecheios() {
+    try {
+      const responseRecheios = await api.get('recheios');
+
+      if (responseRecheios.data) {
+        setMenu(responseRecheios.data);
+      }
+    } catch (error) {
+      toast.error('Falha ao buscar os dados.');
+    }
+  }
+
   useEffect(() => {
-    setMenu([
-      {
-        id: 1,
-        src: 'http://www.diadepizza.com.br/uploads/pizzas/alcatra-c-cheddar-1605571967.png',
-        name: 'ALCATRA C/ CHEDDAR',
-        desc: 'Molho de tomate, alcatra, mussarela, cheddar, azeitonas e orégano.',
-      },
-      {
-        id: 2,
-        src: 'http://www.diadepizza.com.br/uploads/pizzas/nome-aqui-1466684168.png',
-        name: 'AMERICANA',
-        desc: 'Molho de tomate, presunto, ovos, mussarela, bacon, orégano e azeitonas.',
-      },
-      {
-        id: 3,
-        src: 'http://www.diadepizza.com.br/uploads/pizzas/nome-aqui-1466684517.png',
-        name: 'ATUM',
-        desc: 'Molho de tomate, mussarela, atum, tomate, orégano e azeitonas ( cebola opcional)',
-      },
-      {
-        id: 4,
-        src: 'http://www.diadepizza.com.br/uploads/pizzas/nome-aqui-1466684610.png',
-        name: 'BACON',
-        desc: 'Molho de tomate, mussarela, bacon fatiado, orégano e azeitonas.',
-      },
-      {
-        id: 5,
-        src: 'http://www.diadepizza.com.br/uploads/pizzas/bacon-c-milho-1605572045.png',
-        name: 'BACON C/ MILHO',
-        desc: 'Molho de tomate, mussarela, milho, requeijão, bacon fatiado, azeitonas e orégano.',
-      },
-      {
-        id: 6,
-        src: 'http://www.diadepizza.com.br/uploads/pizzas/baiana-1605572190.png',
-        name: 'BAIANA',
-        desc: 'Molho apimentado de carne seca, mussarela, pimentão, cebola, pimenta, azeitonas e orégano.',
-      },
-    ]);
+    getRecheios();
   }, []);
 
   return (
